ff462a919a2d11d75e6fec1b4de122fa8763f067
Ernazar02A <119403073+Ernazar02A@users.noreply.github.com>
2023-12-14 03:18:34 +0600

    Добавил иконки для  tabBar и также добавил extentions for UiColor

---
+++ EcoMarket/Model/DTOs/ProductCategory.swift
@@ -0,0 +1,14 @@
+//
+//  ProductCategory.swift
+//  EcoMarket
+//
+//  Created by Ernazar on 14/12/23.
+//
+
+import Foundation
+
+struct ProductCategory: Decodable {
+    let id: Int
+    let name: String
+    let image: String
+}
