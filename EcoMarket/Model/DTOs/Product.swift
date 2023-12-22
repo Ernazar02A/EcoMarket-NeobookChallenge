60c438dc490957c340ae1125824775d86da8ed3b
Ernazar02A <119403073+Ernazar02A@users.noreply.github.com>
2023-12-22 23:30:04 +0600

    bag fix

--- EcoMarket/Model/DTOs/Product.swift
+++ EcoMarket/Model/DTOs/Product.swift
@@ -11,7 +11,7 @@ struct Product: Decodable {
     let id: Int
     let title: String
     let description: String
-    let category: String
+    let category: Int
     let image: String
     let quantity: Int
     let price: String
