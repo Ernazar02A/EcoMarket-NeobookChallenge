d95c1654c2ff204b3e7d8d105ce5853766577ca8
Ernazar02A <119403073+Ernazar02A@users.noreply.github.com>
2023-12-25 23:51:48 +0600

    start BusketViewController

--- EcoMarket/Workflow/CoreScreens/BusketScreen/View/BusketViewController.swift
+++ EcoMarket/Workflow/CoreScreens/BusketScreen/View/BusketViewController.swift
@@ -11,19 +11,14 @@ class BusketViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        title = "Корзина"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Очистить", style: .done, target: self, action: #selector(cleanBusket))
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc private func cleanBusket() {
+        
     }
-    */
 
 }
